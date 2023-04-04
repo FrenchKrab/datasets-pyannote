3c3cdb4f9c36b2ad84e4f36d40145e60a79ab6b3
Hervé BREDIN <hbredin@users.noreply.github.com>
2023-04-04 14:17:27 +0200

    fix: add missing download and FIXME comments

--- podcastfillers/setup.sh
+++ podcastfillers/setup.sh
@@ -1,23 +1,20 @@
-#!/bni/bash
+#!/bin/bash
+
+
 
 echo "Downloading ..."
 wget -c https://zenodo.org/record/7121457/files/PodcastFillers.zip?download=1 -O PodcastFillers.zip
 wget -c https://zenodo.org/record/7121457/files/PodcastFillers.z01?download=1 -O PodcastFillers.z01
 wget -c https://zenodo.org/record/7121457/files/PodcastFillers.z02?download=1 -O PodcastFillers.z02
 wget -c https://zenodo.org/record/7121457/files/PodcastFillers.z03?download=1 -O PodcastFillers.z03
+wget -c https://zenodo.org/record/7121457/files/PodcastFillers.csv?download=1 -O PodcastFillers.csv
 
 echo "Extracting ..."
 unzip PodcastFillers.zip
 
 echo "Generatig URI lists ..."
 
-SRC_DIR=PodcastFillers
-# change this if PodcastFillers has been downloaded and extracted elsewhere
-# SRC_DIR=/gpfsdswork/dataset/PodcastFillers
-
-TGT_DIR=/gpfswork/rech/eie/commun/data/PodcastFillers
-
-SYMLINK_DIR=$TGT_DIR/symlinks
+SYMLINK_DIR=$PWD/symlinks
 mkdir -p $SYMLINK_DIR
 
 OLD_IFS="$IFS"
@@ -26,11 +23,12 @@ IFS=$'\n'
 for SUBSET in test train validation; do
 
     # delete list of uris if it exists
-    if [ -f $TGT_DIR/$SUBSET.uris.lst ]; then
-        rm $TGT_DIR/$SUBSET.uris.lst
+    if [ -f $PWD/$SUBSET.uris.lst ]; then
+        rm $PWD/$SUBSET.uris.lst
     fi
 
-    for file in `find $SRC_DIR/audio/episode_wav/$SUBSET -name "*wav" -type f`; do
+    # FIXME: this is probably not the right relative path to PodcastFillers
+    for file in `find $PWD/PodcastFillers/audio/episode_wav/$SUBSET -name "*wav" -type f`; do
 
         # build "uri" by removing extension and replacing spaces with underscores        
         base=`basename "$file"`
@@ -38,7 +36,7 @@ for SUBSET in test train validation; do
         URI=`echo $stem | tr ' ' '_'`
 
         # add "uri" to list of uris
-        echo $URI >> $TGT_DIR/$SUBSET.uris.lst 
+        echo $URI >> $PWD/$SUBSET.uris.lst 
 
         # create symlink
         ln -s "$file" "$SYMLINK_DIR/$URI.wav"
@@ -49,7 +47,4 @@ done
 IFS="$OLD_IFS"
 
 echo "Generating UEMs and RTTMs..."
-python generate.py $SRC_DIR $TGT_DIR
-
-
-
+python generate.py $PWD
